a1a1a8076cbfa564ae1605151462daa1d0cfda8f
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-08-02 15:07:43 +0200

    contact page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import Education from "@/components/Education";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
 
 export default function Home() {
   return (
@@ -23,6 +24,9 @@ export default function Home() {
       </section>
       <section id="projects" className="snap-start">
         <Projects />
+      </section>
+      <section id="contact" className="snap-start">
+        <ContactMe />
       </section>
     </div>
   );
