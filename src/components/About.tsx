13973fcc0f771b10c5ea1a79566207a009418ba2
alirezarastineh <kouroshrastineh95@gmail.com>
2023-08-10 13:24:22 +0200

    padding changed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,7 +12,7 @@ export default function About({}: Props) {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       viewport={{ once: true }}
-      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center overflow-hidden"
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-3 sm:px-10 justify-evenly mx-auto items-center overflow-hidden"
     >
       <h3 className="absolute top-16 sm:top-16 md:top-16 lg:top-12 xl:top-12 2xl:top-20 uppercase tracking-[20px] text-gray-500 text-2xl z-10">
         Über
