4356c191c301caf1726c4c14e3a5d807e63a8f34
alirezarastineh <kouroshrastineh95@gmail.com>
2023-08-15 13:22:43 +0200

    sie of h3 changed

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -18,7 +18,7 @@ export default function Skills({}: Props) {
       <h3 className="absolute top-16 sm:top-16 md:top-16 lg:top-12 xl:top-12 2xl:top-20 uppercase tracking-[20px] text-gray-500 text-2xl z-10">
         Skills
       </h3>
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm xl:top-28 2xl:top-56 text-center">
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm sm:text-base xl:top-28 2xl:top-56 text-center">
         Hovern Sie über eine Fähigkeit, um die aktuelle Kenntnisstufe zu sehen
       </h3>
       <div className="xl:mt-48 grid grid-cols-4 2xl:grid-cols-5 gap-5 top-20 sm:top-14 relative">
