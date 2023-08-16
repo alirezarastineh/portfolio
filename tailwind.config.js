acf10045c16d3f174f1c571e1cc0607add82ce5c
alirezarastineh <kouroshrastineh95@gmail.com>
2023-08-16 12:32:07 +0200

    deleted unused configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{js,ts,jsx,tsx,mdx}"];
 export const theme = {
-  extend: {
-    backgroundImage: {
-      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      "gradient-conic":
-        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-    },
-    colors: {
-      "custom-yellow": "#F7AB0A",
-      "custom-gray": "#242424",
-    },
-  },
+  extend: {},
 };
 export const plugins = [];
