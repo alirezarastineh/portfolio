31e4e01e863dd49ffee8b253881b78457a306013
alirezarastineh <kouroshrastineh95@gmail.com>
2024-02-01 00:22:54 +0100

    small changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,7 @@ export default function About({}: Props) {
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-3 sm:px-10 justify-evenly mx-auto items-center overflow-hidden"
     >
       <h3 className="absolute top-16 sm:top-16 md:top-16 lg:top-12 xl:top-12 2xl:top-20 uppercase tracking-[20px] text-gray-500 text-2xl z-10">
-        Über
+        Über Mich
       </h3>
       <motion.div
         initial={{
--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -24,7 +24,7 @@ export default function Education({}: Props) {
       className="flex flex-col relative h-screen text-left md:text-left md:flex-row max-w-7xl px-7 sm:px-10 justify-evenly mx-auto items-center overflow-hidden"
     >
       <h3 className="absolute top-16 sm:top-16 md:top-16 lg:top-12 xl:top-12 2xl:top-20 uppercase tracking-[20px] text-gray-500 text-2xl z-10">
-        Bildung
+        Bildung & Erfahrung
       </h3>
       <Swiper
         spaceBetween={350}
@@ -47,8 +47,8 @@ export default function Education({}: Props) {
       >
         <SwiperSlide>
           <EducationCard
-            position="Full-Stack-Entwicklung Weiterbildung"
-            company="Code Academy Berlin"
+            position="Full-Stack-Entwicklung Bootcamp"
+            company="Code Academy Berlin Education GmbH"
             imageUrl="https://i.imgur.com/QIe70hH.png"
             summaryPoints={[
               "Vollzeit",
